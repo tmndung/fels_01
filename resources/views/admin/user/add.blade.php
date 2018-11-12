7e4cf7bef7de75bf5b87b21d8d8202b119c3116c
Dung Tran <tmndung@gmail.com>
2018-11-12 10:31:46 +0700

    ac

--- resources/views/admin/user/add.blade.php
+++ resources/views/admin/user/add.blade.php
@@ -1,64 +1,63 @@
 @extends('layout.admin.master')
 
 @section('css')
-{{ Html::style(asset('css/toastr.min.css')) }}
+    {{ Html::style(asset('css/toastr.min.css')) }}
 @endsection
 
 @section('content')
-<div class="content-wrapper">
-    <section class="content-header">
-        <h1>
-            @lang('message.lte.user')
-            <small
-            ">@lang('message.lte.add')</small>
-        </h1>
-        <ol class="breadcrumb">
-            <li><a href="#"><i class="fa fa-dashboard"></i>@lang('message.lte.home_page')</a></li>
-            <li class="active"><a href="">@lang('message.lte.user')</a></li>
-        </ol>
-    </section>
+    <div class="content-wrapper">
+        <section class="content-header">
+            <h1>
+                @lang('message.lte.user')
+                <small
+                ">@lang('message.lte.add')</small>
+            </h1>
+            <ol class="breadcrumb">
+                <li><a href="#"><i class="fa fa-dashboard"></i>@lang('message.lte.home_page')</a></li>
+                <li class="active"><a href="">@lang('message.lte.user')</a></li>
+            </ol>
+        </section>
 
-    <section class="content">
-        <div class="container">
-            <div class="row">
-                @if($errors->any())
-                    <div class="col-md-12">
-
-                        <div class="alert alert-danger">
-                            <ul style="list-style-type: none">
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
+        <section class="content">
+            <div class="container">
+                <div class="row">
+                    @if($errors->any())
+                        <div class="col-md-12">
+                            <div class="alert alert-danger">
+                                <ul style="list-style-type: none">
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
                         </div>
+                    @endif
+                    <div class="col-md-12">
                     </div>
-                @endif
+                </div>
+            </div>
+
+            <div class="row">
+                {!! Toastr::message() !!}
                 <div class="col-md-12">
+                    <div class="box box-primary">
+                        <div class="box-header with-border">
+                            <h3 class="box-title">@lang('message.lte.quick_ex')</h3>
+                        </div>
+
+                        {!! Form::open(['url' => url('user'),'method' => 'post','files'=>true]) !!}
+                        <div class="box-body">
+                            @include('form.user.user')
+                        </div>
+                        <div class="box-footer">
+                            <button type="submit" class="btn btn-primary">@lang('message.lte.add')</button>
+                        </div>
+                        {!! Form::close() !!}
+                    </div>
                 </div>
             </div>
-        </div>
-
-        <div class="row">
-            {!! Toastr::message() !!}
-            <div class="col-md-12">
-                <div class="box box-primary">
-                    <div class="box-header with-border">
-                        <h3 class="box-title">@lang('message.lte.quick_ex')</h3>
-                    </div>
-
-                    {!! Form::open(['url' => url('user'),'method' => 'post','files'=>true]) !!}
-                    <div class="box-body">
-                        @include('form.user.user')
-                    </div>
-                    <div class="box-footer">
-                        <button type="submit" class="btn btn-primary">@lang('message.lte.add')</button>
-                    </div>
-                    {!! Form::close() !!}
-                </div>
-            </div>
-        </div>
-    </section>
-</div>
+        </section>
+    </div>
 @endsection
 
 @section('script')
--- resources/views/admin/user/list.blade.php
+++ resources/views/admin/user/list.blade.php
@@ -1,76 +1,76 @@
 @extends('layout.admin.master')
 @section('css')
-<link rel="stylesheet" href="http://cdn.bootcss.com/toastr.js/latest/css/toastr.min.css">
-<script src="http://cdn.bootcss.com/toastr.js/latest/js/toastr.min.js"></script>
-
+    <link rel="stylesheet" href="http://cdn.bootcss.com/toastr.js/latest/css/toastr.min.css">
+    <script src="http://cdn.bootcss.com/toastr.js/latest/js/toastr.min.js"></script>
 @endsection
 
 @section('content')
     <div class="content-wrapper">
-      <section class="content-header">
-        <h1>
-          @lang('message.lte.list_user')
-        </h1>
-        <ol class="breadcrumb">
-          <li><a href="#"><i class="fa fa-dashboard"></i>@lang('message.lte.home_page')</a></li>
-          <li><a href="">@lang('message.lte.user')</a></li>
-        </ol>
-      </section>
+        <section class="content-header">
+            <h1>
+                @lang('message.lte.list_user')
+            </h1>
+            <ol class="breadcrumb">
+                <li><a href="#"><i class="fa fa-dashboard"></i>@lang('message.lte.home_page')</a></li>
+                <li><a href="">@lang('message.lte.user')</a></li>
+            </ol>
+        </section>
 
-      <section class="content">
-        <div id="dialog-confirm" title="@lang('message.lte.message')" class="dia_title">
-          <p>@lang('message.lte.delete_element')</p>
-        </div>
-      <div class="row">
-      {!! Toastr::message() !!}
-      <div class="col-xs-12">
-      {!! Form::open(['method' => 'GET', 'id' => 'form-search']) !!}
-        <div class="form-group">
-          {!! Form::text('keyword',null, ['id' => 'search', 'placeholder' => trans('message.lte.search_name')]) !!}
-        </div>
-      {!! Form::close() !!}
-        <div class="box">
-          <div class="box-header">
-            <h3 class="box-title"><a class="btn btnsty" href="{{url('admin/user/add')}}" ">@lang('message.lte.add_user')</a></h3>
-          </div>
-            <div class="box-body">
-              <div id="result">
-                <table id="example2" class="table table-bordered table-striped animated fadeIn">
-                  <thead style="" >
-                    <tr>
-                      <th class="th_lte">@lang('message.lte.stt')</th>
-                      <th class="th_lte" >@lang('message.lte.name')</th>
-                      <th class="th_lte">@lang('message.lte.avata')</th>
-                      <th class="th_lte">@lang('message.lte.email')</th>
-                      <th class="th_lte">@lang('message.lte.address')</th>
-                      <th class="th_lte">@lang('message.lte.phone')</th>
-                      <th class="th_lte">@lang('message.lte.action')</th>
-                    </tr>
-                 </thead>
-                  <tbody>
-                    @foreach($users as $user)
-                    <tr id="user_list">
-                      <td >{{$user->id}}</td>
-                      <td>{{$user->name}}</td>
-                      <td>{{$user->avata}}</td>
-                      <td>{{$user->email}}</td>
-                      <td>{{$user->address}}</td>
-                      <td>{{$user->phone}}</td>
-                      <td class="tdlist"><a class="colorres" href=""><i class="fa fa-trash "></i></a>
-                        <a class="colorgreen"  href=""><i class="fa fa-edit"></i></a>
-                      </td>
-                    </tr>
-                  @endforeach
-                  </tbody>
-                </table>
-                <div class="pagination" >
-                  {{ $users->links() }}
-                </div>
-              </div>
+        <section class="content">
+            <div id="dialog-confirm" title="@lang('message.lte.message')" class="dia_title">
+                <p>@lang('message.lte.delete_element')</p>
             </div>
-          </div>
-        </div>
-      </div>
-      </section>
+            <div class="row">
+                {!! Toastr::message() !!}
+                <div class="col-xs-12">
+                    {!! Form::open(['method' => 'GET', 'id' => 'form-search']) !!}
+                    <div class="form-group">
+                        {!! Form::text('keyword',null, ['id' => 'search', 'placeholder' => trans('message.lte.search_name')]) !!}
+                    </div>
+                    {!! Form::close() !!}
+                    <div class="box">
+                        <div class="box-header">
+                            <h3 class="box-title"><a class="btn btnsty" href="{{url('admin/user/add')}}"
+                                ">@lang('message.lte.add_user')</a></h3>
+                        </div>
+                        <div class="box-body">
+                            <div id="result">
+                                <table id="example2" class="table table-bordered table-striped animated fadeIn">
+                                    <thead style="">
+                                    <tr>
+                                        <th class="th_lte">@lang('message.lte.stt')</th>
+                                        <th class="th_lte">@lang('message.lte.name')</th>
+                                        <th class="th_lte">@lang('message.lte.avata')</th>
+                                        <th class="th_lte">@lang('message.lte.email')</th>
+                                        <th class="th_lte">@lang('message.lte.address')</th>
+                                        <th class="th_lte">@lang('message.lte.phone')</th>
+                                        <th class="th_lte">@lang('message.lte.action')</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    @foreach($users as $user)
+                                        <tr id="user_list">
+                                            <td>{{$user->id}}</td>
+                                            <td>{{$user->name}}</td>
+                                            <td>{{$user->avata}}</td>
+                                            <td>{{$user->email}}</td>
+                                            <td>{{$user->address}}</td>
+                                            <td>{{$user->phone}}</td>
+                                            <td class="tdlist"><a class="colorres" href=""><i class="fa fa-trash "></i></a>
+                                                <a class="colorgreen" href=""><i class="fa fa-edit"></i></a>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                                <div class="pagination">
+                                    {{ $users->links() }}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
 @endsection
